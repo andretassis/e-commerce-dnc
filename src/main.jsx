c827a555365d44c78385a7d37716d252a0b26673
andretassis <tassisandre@gmail.com>
2023-12-11 12:04:10 -0300

    chore: estruturação das pastas, imagens e variaveis do sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.scss'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/home",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/login",
+    element: <div>Login</div>,
+  },
+  {
+    path: "/product/:productID",
+    element: <div>Tela de produtos</div>,
+  },
+  {
+    path: "/pay/:payID",
+    element: <div>Tela de pagamento</div>,
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
